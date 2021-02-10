9307e68abbd603a4a1d9a1c1733088a57ed52baa
Larry Ioannidis <larry@rakun.ie>
2021-02-10 21:53:55 +0000

    Fix footer positioning

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     id="app"
-    class="font-mono"
+    class="font-mono max-h-screen fixed top-0 left-0 right-0 bottom-0"
   >
     <header
       class="fixed flex justify-center items-center w-full top-0 left-0 h-14 bg-black text-white"
@@ -14,7 +14,7 @@
       </router-link>
     </header>
     <main class="fixed z-10 top-14 bottom-14 left-0 w-full">
-      <router-view class="bg-white" />
+      <router-view class="bg-transparent" />
     </main>
     <footer
       v-if="$route.meta.requiresAuth"
